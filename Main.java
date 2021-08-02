ce7e136e68ca4ede486d363e546d3a3dea457fc9
ashwinnirmul <>
2021-08-02 21:31:06 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+class Main {
+  public static void main(String[] args) {
+    
+    String code;
+    code = "10100111001";
+
+    manchesterCode(code);
+
+  }
+
+  public static void manchesterCode(String code)
+  { char c;
+    for( int i = 0; i < code.length(); i++ )
+    {
+      c = code.charAt(i);
+      //System.out.println(c);
+      if(c=='1')
+        System.out.print("10");
+      else
+        System.out.print("01");
+    }
+    
+    System.out.println();
+  }
+}
